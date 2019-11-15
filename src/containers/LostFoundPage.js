1eeb7506e4d864302d979083d72e936c4966a05d
github username <github email address>
2019-11-14 21:51:54 -0700

    add and design pet profile form

--- src/containers/LostFoundPage.js
+++ src/containers/LostFoundPage.js
@@ -18,33 +18,50 @@ export class LostFoundPage extends Component {
                 </div>
                 <form className="create-profile" onSubmit={this.handleSubmit}>
                     <h2>Create A Pet Profile</h2>
-                    <div className="field">
-                        <label className="field-label">First Name</label>
-                        <input type="text" name="firstName" onChange={this.handleChange}/>
-                    </div>
-                    <div className="field">
-                        <label className="field-label">Last Name</label>
-                        <input type="text" name="lastName" onChange={this.handleChange}/>
-                    </div>
-                    <div className="field">
-                        <label className="field-label">Phone Number</label>
-                        <input type="text" name="phoneNumber" onChange={this.handleChange}/>
-                    </div>
-                    <div className="field">
-                        <label className="field-label">Email</label>
-                        <input type="text" name="email" onChange={this.handleChange}/>
-                    </div>
-                    <div className="field">
-                        <label className="field-label">Password</label>
-                        <input type="password" name="password" onChange={this.handleChange}/>
-                    </div>
-                    {/* <div className="field">
-                        <label className="field-label">Confirm Password</label>
-                        <input type="password" name="password"/>
-                    </div> */}
-                    <input id="submit" type="submit" value="Submit"/>
-                    <div className="login-link">
-                        <p onClick={this.formLink}>Already a user?</p>
+
+                    <div className="profile-info">
+                        <div className="pet-image-box">
+                            <img className="pet-photo" src="https://vippuppies.com/wp-content/uploads/2019/04/Kurstin-497AEB12-4825-46A6-81E2-6073471430D0.jpeg" alt="bulldog"/>
+                        </div>
+                        <div className="form-box">
+                            <div className="field">
+                                <label className="field-label">Name</label>
+                                <input type="text" name="name" onChange={this.handleChange}/>
+                            </div>
+                            <div className="field">
+                                <label className="field-label">Species</label>
+                                <input type="text" name="species" onChange={this.handleChange}/>
+                            </div>
+                            <div className="field">
+                                <label className="field-label">Gender</label>
+                                <input type="text" name="gender" onChange={this.handleChange}/>
+                            </div>
+                            <div className="field">
+                                <label className="field-label">Size</label>
+                                <input type="text" name="size" onChange={this.handleChange}/>
+                            </div>
+                            <div className="field">
+                                <label className="field-label">Color</label>
+                                <input type="text" name="color" onChange={this.handleChange}/>
+                            </div>
+                            <div className="field">
+                                <label className="field-label">Age</label>
+                                <input type="text" name="age" onChange={this.handleChange}/>
+                            </div>
+                            <div className="field">
+                                <label className="field-label">Breed</label>
+                                <input type="text" name="breed" onChange={this.handleChange}/>
+                            </div>
+                            <div className="field">
+                                <label className="field-label">Temperament</label>
+                                <input type="text" name="temperament" onChange={this.handleChange}/>
+                            </div>
+                            <div className="comments">
+                                <label className="field-label">Comments</label>
+                                <input type="text" name="comments" onChange={this.handleChange}/>
+                            </div>
+                        </div>
+                        <input id="submit" type="submit" value="Submit"/>
                     </div>
                 </form>
             </div>
